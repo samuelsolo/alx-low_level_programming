eeb1ea6dd1444d3c08eb3cef65d1a4ede9141b0f
samuelsolo <samuelsolomondes@gmail.com>
2023-10-06 18:44:57 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - does something
+ * Description: prints alphabet
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		if (letter != 'q' && letter != 'e')
+		{
+			putchar(letter);
+			letter++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}

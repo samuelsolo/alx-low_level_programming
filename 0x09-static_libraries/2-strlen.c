8db57ff32fc204f98193e72368b0400c9e9d4582
samuelsolo <samuelsolomondes@gmail.com>
2023-10-28 18:03:52 -0700

    add stuff

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -10,7 +10,7 @@ int _strlen(char *s)
 {
 	int len;
 
-	for (len = 0; s[len] != '\0'; len++);
+	for (len = 0; s[len] != '\0'; len++)
 
 	return (len);
 }
--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -7,17 +7,17 @@
 */
 
 char *_strncpy(char *dest, const char *src, size_t n)
+
 {
 	size_t i;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
-	
 	{
-		dest[i] = src[i];
+	dest[i] = src[i];
 	}
 	for (; i < n; i++)
 	{
-		dest[i] = '\0';
+	dest[i] = '\0';
 	}
 
 	return (dest);
